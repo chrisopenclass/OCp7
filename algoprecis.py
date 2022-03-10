b0856401f3a09d42bb470636c19adc2d7655001a
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-03-10 16:25:32 +0100

    nouvelle approche
    sauvegarde de l'ancien script optimiser  précis , modification de l'ancien algo optimizer  les nombres sont arroundi et non plu multiplier par 100

--- algoprecis.py
+++ algoprecis.py
@@ -2,17 +2,15 @@ import csv
 from collections import namedtuple
 import time
 
-start = time.time()
-
 with open('dataset2_python+p7(1).csv', mode='r') as fichiercsv:
     reader = csv.reader(fichiercsv)
     next(reader)
-    dictionnaire = {rows[0]: [round(float(rows[1])), float(rows[2]),
+    dictionnaire = {rows[0]: [int(float(rows[1])*100), float(rows[2]),
                     float(rows[1]) * float(rows[2])/100]
-                    for rows in reader if round(float(rows[1])) > 0}
+                    for rows in reader if float(rows[1])*100 > 0}
 
 
-portefeuille = 500
+portefeuille = 500 * 100
 
 
 Action = namedtuple("Action", "nom,prix,valeur")
@@ -25,6 +23,7 @@ for action in dictionnaire:
 
 tableau = [[0 for x in range(portefeuille + 1)]for x in range(len(liste) + 1)]
 
+start = time.time()
 
 for t in range(1, len(liste)+1):
     for solde_actuel in range(portefeuille):
@@ -33,6 +32,8 @@ for t in range(1, len(liste)+1):
         valeur_total = tableau[t-1][solde_actuel - action_consideree.prix] + action_consideree.valeur
         if solde_actuel >= action_consideree.prix and tableau[t][solde_actuel] < valeur_total:
             tableau[t][solde_actuel] = valeur_total
+
+print(f" le temps d'exécussion est de : {time.time() - start} seconde")
 
 
 def fonction(tableau):
@@ -52,7 +53,7 @@ def fonction(tableau):
             i -= 1
     for element in liste_a_acheter:
         action.append(element.nom)
-        cout_total += element.prix
+        cout_total += element.prix / 100
         benef += element.valeur
     print(f'les action à acheter sont :{action}')
     print(f'pour un cout total de :{cout_total}')
@@ -60,5 +61,3 @@ def fonction(tableau):
 
 
 action_a_acheter = fonction(tableau)
-
-print(f" le temps d'exécussion est de : {time.time() - start} seconde")
