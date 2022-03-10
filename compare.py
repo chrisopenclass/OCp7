3f5764e9b9d541f5603480e6379fe1c5fff7549f
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-03-10 17:04:45 +0100

    outils de comparaison des listes

---
+++ compare.py
@@ -0,0 +1,31 @@
+def is_identical(list_a, list_b):
+    if len(list_a) != len(list_b):
+        print(f"la liste A contient {len(list_a)} element alors que la liste B contient {len(list_b)} elements\n")
+    else:
+        print("les deux liste font la même taille\n")
+    print("les éléments suivants sont présents dans la liste A mais pas dans la B :")
+    for i in list_a:
+        if i not in list_b:
+            print(f"{i}")
+    print("")
+    print("les éléments suivant sont présent dans la liste B mais pas dans la A : ")
+    for i in list_b:
+        if i not in list_a:
+            print(f"{i}")
+    print("")
+    print("les éléments suivant sont présent dans les deux listes : ")
+    for i in list_a:
+        if i in list_b:
+            print(f"{i}")
+
+
+list_a = ['Share-IXCI', 'Share-FWBE', 'Share-ZOFA', 'Share-PLLK', 'Share-MEQV',
+                        'Share-LXZU', 'Share-PSMF', 'Share-PATS', 'Share-SCWM', 'Share-VCXT', 'Share-NDKR',
+                        'Share-ALIY', 'Share-JWGF', 'Share-FCHD', 'Share-JGTW', 'Share-VCAX', 'Share-LFXB',
+                        'Share-DWSK', 'Share-XQII', 'Share-OAVO', 'Share-ROOM', 'Share-YCGH']
+list_b = ['Share-ECAQ', 'Share-IXCI', 'Share-FWBE', 'Share-ZOFA', 'Share-PLLK',
+                        'Share-LXZU', 'Share-YFVZ', 'Share-ANFX', 'Share-PATS', 'Share-SCWM', 'Share-NDKR',
+                        'Share-ALIY', 'Share-JWGF', 'Share-JGTW', 'Share-FAPS', 'Share-VCAX', 'Share-LFXB',
+                        'Share-DWSK', 'Share-XQII', 'Share-ROOM']
+
+is_identical(list_a, list_b)
